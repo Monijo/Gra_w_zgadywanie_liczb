7573bc35a43c4cfa69de4a541dfa9aca0150f64c
Monijo <mokajozefowicz@gmail.com>
2021-10-23 14:22:10 +0200

    except ValueError

--- game.py
+++ game.py
@@ -4,7 +4,11 @@ def game_of_guessing_numbers():
     drawn_number = randint(1,100)
     guessing_number = 0
     while not(drawn_number == guessing_number):
-    guessing_number = input("Guess the number: ")
-        if not(isinstance(guessing_number, int)):
+        try:
+            guessing_number = int(input("Guess the number: "))
+        except ValueError:
             print("It's not a number!")
 
+
+
+game_of_guessing_numbers()
