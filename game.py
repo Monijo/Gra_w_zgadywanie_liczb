e3f593a28879389be4ef97b63581994119d177b2
Monijo <mokajozefowicz@gmail.com>
2021-10-23 14:07:14 +0200

    type checking

--- game.py
+++ game.py
@@ -2,5 +2,9 @@ from random import randint
 
 def game_of_guessing_numbers():
     drawn_number = randint(1,100)
+    guessing_number = 0
+    while not(drawn_number == guessing_number):
     guessing_number = input("Guess the number: ")
+        if not(isinstance(guessing_number, int)):
+            print("It's not a number!")
 
